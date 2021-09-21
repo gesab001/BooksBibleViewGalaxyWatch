8cb80a17b3cd4439b90c487607ffcf719d6a042a
gesab001 <144000giovanni@gmail.com>
2021-09-21 21:15:11 +1200

    update

--- arabic.py
+++ arabic.py
@@ -19,6 +19,8 @@ item = {"book": name, "chapter": chapter, "verse": verseNumber, "word": word}
 books = {}
 print(item)
 """
+
+"""
 jsonData = {}
 for book in data:
   name = book['name']
@@ -46,7 +48,29 @@ for book in data:
       jsonData[bookTitle].append(item)
 
 print(jsonData)     
-with open("arabicbible.json",  'w', encoding='utf8') as f:
+with open("./arabicbible/arabicbible.json",  'w', encoding='utf8') as f:
   json.dump(jsonData, f, ensure_ascii=False)
-      
+"""      
     
+for book in data:
+  chapters = book['chapters']
+  #print(len(chapters))
+  bookTitle = book['name']
+  #print(bookTitle)
+  for x in range (0,len(chapters)):
+    #print(len(chapter))
+    chapterNumber = x+1
+    #print("chapter " + chapterNumber )
+    verses = chapters[x]
+    #print("total verses " + str(len(verses)))
+    for x in range (0, len(verses)):
+      verseNumber = x+1
+      word = verses[x]
+      #print(str(verseNumber) + ". " + word)
+      item = {"book": bookTitle, "chapter": chapterNumber, "verse": verseNumber, "word": word}
+      #print(item)
+      #jsonData[bookTitle].append(item)
+      filename = bookTitle.replace(" ", "") + str(chapterNumber) + str(verseNumber) + ".json"
+      print(filename)
+      with open("./arabicbible/"+filename,  'w', encoding='utf8') as f:
+        json.dump(item, f, ensure_ascii=False)
