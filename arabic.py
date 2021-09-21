fedd57c65440739ede6213ded373cc3a6e213326
gesab001 <144000giovanni@gmail.com>
2021-09-21 20:56:55 +1200

    update

---
+++ arabic.py
@@ -0,0 +1,52 @@
+import json
+
+f = open("arabic.json", "r", encoding="utf-8")
+data = json.loads(f.read())
+"""
+i = 0
+book = data[i]
+name = book['name']
+chapters = book['chapters']
+numberOfChapters = len(chapters)
+chapter = 1
+print(book)
+chapterText = chapters[chapter]
+print(chapterText)
+print(numberOfChapters)
+verseNumber = 1
+word = chapterText[verseNumber]
+item = {"book": name, "chapter": chapter, "verse": verseNumber, "word": word}
+books = {}
+print(item)
+"""
+jsonData = {}
+for book in data:
+  name = book['name']
+  print(name)  
+  jsonData[name] = []
+
+print(jsonData)
+for book in data:
+  chapters = book['chapters']
+  #print(len(chapters))
+  bookTitle = book['name']
+  #print(bookTitle)
+  for x in range (0,len(chapters)):
+    #print(len(chapter))
+    chapterNumber = x+1
+    #print("chapter " + chapterNumber )
+    verses = chapters[x]
+    #print("total verses " + str(len(verses)))
+    for x in range (0, len(verses)):
+      verseNumber = x+1
+      word = verses[x]
+      #print(str(verseNumber) + ". " + word)
+      item = {"book": bookTitle, "chapter": chapterNumber, "verse": verseNumber, "word": word}
+      #print(item)
+      jsonData[bookTitle].append(item)
+
+print(jsonData)     
+with open("arabicbible.json",  'w', encoding='utf8') as f:
+  json.dump(jsonData, f, ensure_ascii=False)
+      
+    
